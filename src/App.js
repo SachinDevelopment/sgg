955705000ead64a3687acbabfadfb43ed1797b3c
SachinDevelopment <53714757+SachinDevelopment@users.noreply.github.com>
2022-05-28 14:46:58 -0400

    auth0 configured

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import { useState, useEffect } from 'react'
+import { useAuth0 } from "@auth0/auth0-react";
 
 import './App.css';
 import axios from 'axios'
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
 
 // import { BrowserRouter, Route, Routes  } from 'react-router-dom';
 
-let API_URL = "http://localhost:5000";
-if(process.env.NODE_ENV === 'production'){
-  API_URL = process.env.REACT_APP_API_URL;
-}
+let API_URL = process.env.REACT_APP_API_URL;
 
 const App = () => {
   const [msg, setMsg] = useState('');
+  const { user, isAuthenticated, isLoading } = useAuth0();
 
   useEffect (() => {
     axios.get(`${API_URL}/health`).then(({ data }) => {
@@ -20,11 +21,32 @@ const App = () => {
     });
   }, [])
 
-  return (
-    <div className="wrapper">
-      {`Application: ${msg}`}
-    </div>
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  if(isAuthenticated){
+    return (
+      <div className="flex justify-center">
+        <div>
+          <img src={user.picture} alt={user.name} />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+        <LogoutButton/>
+      </div>
+      
   );
+  } else {
+    return (
+      <div className="flex justify-center items-center h-full space-x-2">
+        <div>
+        {`API Response: ${msg}`}
+        </div>
+        <LoginButton/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+      <Auth0Provider
+    domain="dev-gtsk-v34.us.auth0.com"
+    clientId="trbSfyPR4LbS8jEjiDdFpzOhIxw5QHZJ"
+    redirectUri={window.location.origin}
+    audience="https://dev-gtsk-v34.us.auth0.com/api/v2/"
+    scope="read:current_user update:current_user_metadata"
+  >
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
