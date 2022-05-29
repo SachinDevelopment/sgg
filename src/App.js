54d62ac4e8d6d8909d1ffdf4eff3d99d789f5c61
SachinDevelopment <53714757+SachinDevelopment@users.noreply.github.com>
2022-05-28 20:03:04 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 import Leaderboard from "./components/Leaderboard";
 import { useAuth0 } from "@auth0/auth0-react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Player from './components/Player'
+import Player from "./components/Player";
 
 const App = () => {
   const { isAuthenticated } = useAuth0();
@@ -16,8 +16,9 @@ const App = () => {
         <Header />
         {isAuthenticated ? (
           <Routes className="p-2">
-            <Route path="/leaderboard" element={ <Leaderboard />}/>
-            <Route path="/player/:id/stats" element={  <Player />} exact/>
+            <Route path="/leaderboard" element={<Leaderboard />} />
+            <Route path="/player/:id/stats" element={<Player />} exact />
+            <Route path="/" element={<Leaderboard />} />
           </Routes>
         ) : (
           <div className="p-2">"Login to see stats"</div>
